5d4c2583bb360d2b3a4b00f9628c3ce6627ec9f4
Brody Beebe <bbeebe13@gmail.com>
2021-05-22 01:12:56 -0600

    added storage for high score

--- assets/js/game.js
+++ assets/js/game.js
@@ -147,13 +147,27 @@ var startGame = function() {
 
 var endGame = function () {
 
+    
     //if player is still alive they win
     if (playerInfo.health > 0) {
         window.alert("Great job, you survived the game! You now have a score of " + playerInfo.money + ".");
+        var highScore = localStorage.getItem("highscore");
+        if (highScore === null ) {
+            highScore = 0;
+        }
+        if (playerInfo.money > highScore) {
+            localStorage.setItem("highscore", playerInfo.money);
+            localStorage.setItem("name", playerInfo.name);
+
+            alert(playerInfo.name + " now has the high score of " + playerInfo.money);
+        } else {
+            alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+        }
     }
     else {
         window.alert("You've lost your robot in battle.");
     }
+
 
     //ask player if they want to play again
     var playAgainConfirm = window.confirm("Would you like to play again?");
